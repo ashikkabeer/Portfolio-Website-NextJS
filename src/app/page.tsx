ace1f2d3783d17611945d05a7ad7de02b3426347
Ashik Kabeer <97529548+ashikkabeer@users.noreply.github.com>
2024-01-01 11:11:32 +0530

    about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,13 +15,7 @@ export default function Home() {
       </p>
       <p className='justify-start text-3xl pt-24'>About Ashik Kabeer</p>
       <p className='about mt-4'>
-        Weekend Labs is a software consulting and investment collective based in
-        Boston. Founded in 2021, Weekend Labs is a tight-knit collective of
-        industry insiders, analysts, and innovators. We are open, collaborative,
-        and use our collective experience, design and technical expertise to
-        create meaningful and compelling content for all mediums. We take a
-        hands-on approach to consulting in a way that truly feels like a
-        business partner.
+      Hello! I'm Ashik Kabeer, a dedicated Node.js developer currently pursuing my B.Tech in computer science and engineering in India. With a knack for building scalable backend applications, I've crafted a range of projects that reflect my commitment to innovation and clean coding practices. Beyond coding, I enjoy sharing my insights through technical content creation. As the Tech Lead of TinkerHub MCET Campus Community, I collaborate with like-minded individuals to foster a culture of innovation and learning. Whether you're interested in technology, Node.js, or just want to connect, I'm always open to new opportunities and collaborations. Let's build something amazing together!
       </p>
 
       <p className='justify-start text-3xl pt-24'>My Backend Projects</p>
